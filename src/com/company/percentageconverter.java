b12bac73718ed16dee479019bf1f1c44381d1786
neeraj3161 <neerajtripathi7447@gmail.com>
2023-03-29 17:35:34 +0530

    Added A Programme to Calculate Percentage

---
+++ src/com/company/percentageconverter.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class percentageconverter {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int Science;
+        int Maths;
+        int SocialScience;
+//        int English;
+        int Marathi;
+        int Hindi;
+
+         Science= scanner.nextInt();
+         Maths= scanner.nextInt();
+         SocialScience=scanner.nextInt();
+//        English=scanner.nextInt();
+        Marathi=scanner.nextInt();
+        Hindi=scanner.nextInt();
+
+         int sum=Science+Maths+SocialScience+Marathi+Hindi;
+        System.out.println(sum);
+        System.out.println(sum*100/500);
+
+    }
+}
