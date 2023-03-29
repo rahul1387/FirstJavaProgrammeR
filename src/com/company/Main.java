7c174f86835ff5b7c567bd173141611c9c5f5314
neeraj3161 <neerajtripathi7447@gmail.com>
2023-03-29 16:28:43 +0530

    addition of two numbers using java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.print("The Sum of these Numbers is: ");
 
         int num1=4;
         int num2=8;
         int num3=7;
         int sum= num1 +num2+num3;
+
+        System.out.println("Some change");
 
         System.out.println(sum);
     }
