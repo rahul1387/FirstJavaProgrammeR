ae1fc1abde45cb1d4fa74a6d80acd0227a985c72
neeraj3161 <neerajtripathi7447@gmail.com>
2023-03-29 16:31:07 +0530

    addition of two numbers using java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,9 @@ public class Main {
         int num3=7;
         int sum= num1 +num2+num3;
 
+        //below line is used to print anything inside the println
+        //print means no new line will be added
+        //println means new line will be added
         System.out.println("Some change");
 
         System.out.println(sum);
