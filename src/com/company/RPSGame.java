d4839a4ed4ba8ffc6d3fa7fb3fc4310722cba5ef
neeraj3161 <neerajtripathi7447@gmail.com>
2023-04-01 13:33:43 +0530

    Rock,Paper,Scissor Game By Random

---
+++ src/com/company/RPSGame.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RPSGame {
+    public static void main(String[] args) {
+        //0 for Rock
+        //1 for Paper
+        //2 for Scissor
+
+        Scanner sc = new Scanner(System.in);
+        int shouldExist=1;
+        while (shouldExist == 1)
+        {
+            System.out.print("Enter 0 for Rock,Enter 1 for Paper,Enter 2 for Scissor:");
+            int UserInput = sc.nextInt();
+
+            Random rd = new Random();
+            int computerInput = rd.nextInt();
+
+            if (UserInput == computerInput) {
+                System.out.println("Draw");
+            } else if (UserInput == 0 && computerInput == 2 || UserInput == 1 && computerInput == 0
+                    || UserInput == 2 && computerInput == 1) {
+                System.out.println("You Win!");
+            } else {
+                System.out.println("Computer Win!");
+            }
+            System.out.println("Do You Want to Play Again if yes(press 1) to exit (press 0):");
+            shouldExist = sc.nextInt();
+        }
+    }
+}
