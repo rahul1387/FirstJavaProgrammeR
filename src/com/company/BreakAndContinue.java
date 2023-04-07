7ef5296d9a096b165e8c2f8142ebd649f91db9bc
rahul1387 <rahultripathi7447@gmail.com>
2023-04-07 13:06:39 -0700

    Use of Break and Continue

---
+++ src/com/company/BreakAndContinue.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class BreakAndContinue {
+    public static void main(String[] args) {
+
+//        for (int i=0; i<5; i++){
+//            System.out.println(i);
+//            if (i==2){
+//                System.out.println("Loop is Ending");
+//                break;
+//            }
+//        }
+//        int i=0;
+//        while (i<5) {
+//
+//            System.out.println(i);
+//            if (i == 2) {
+//
+//                System.out.println("loop is Ending");
+//                break;
+//
+//            }
+//            i++;
+
+        for (int j=0; j<5; j++){
+            System.out.println(j);
+            if (j==2){
+                continue;
+            }
+        }
+
+
+
+
+
+    }
+}
